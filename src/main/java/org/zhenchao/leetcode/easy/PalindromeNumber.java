1e15f00d5ddf1ff2dac7294038b9bcdd09c6942c
zhenchao.wang <zhenchao.wang@hotmail.com>
2016-09-11 15:08:44 +0800

    AC：PalindromeNumber

--- src/main/java/org/zhenchao/leetcode/easy/PalindromeNumber.java
+++ src/main/java/org/zhenchao/leetcode/easy/PalindromeNumber.java
@@ -1,44 +1,40 @@
 package org.zhenchao.leetcode.easy;
 
 /**
- * Determine whether an integer is a palindrome.<br/>
- * Do this without extra space.
+ * No.9
  *
- * @author Apache_xiaochao 2015-9-26 11:46:26
+ * @author zhenchao.wang 2016-09-11 13:15
+ * @version 1.0.0
  */
 public class PalindromeNumber {
 
-    public static void main(String[] args) {
-        PalindromeNumber pn = new PalindromeNumber();
-        System.out.println(pn.isPalindrome(10022001));
-    }
-
+    /**
+     * 负数不是回文
+     *
+     * @param x
+     * @return
+     */
     public boolean isPalindrome(int x) {
 
-        if (x < 0) {
-            return false;
-        }
-        if (x >= 0 && x <= 9) {
-            return true;
-        }
+        // 特例处理
+        if (x < 0) return false;  // 负数不是回文
+        if (x >= 0 && x <= 9) return true;
 
-        // 获取一个用于取模的数
-        int base = 10;
-        while (x / base >= 10) {
-            base *= 10;
-        }
+        // 构建除数
+        int divider = 1;
+        while (x / divider >= 10) divider *= 10;
 
+        // 比对校验
         while (x > 0) {
-            int left = x / base;  // 获取头数
-            int right = x % 10;  // 获取尾数
-            if (left != right) {
+            if (x / divider != x % 10) {
                 return false;
             }
-            x -= left * base;  // 去掉已经检测过头数
-            x /= 10; // 去掉已经检测过的尾数
-            base /= 100;  // 因为每次去掉首尾两个数，所以这里需要模100
-
+            x %= divider;
+            x /= 10;
+            divider /= 100;
         }
+
         return true;
     }
+
 }
