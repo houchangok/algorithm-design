f4120f4079ba436c5e24e87a14fe0f93b4cd8bd3
zhenchao <zhenchao.wang@hotmail.com>
2017-04-25 22:37:03 +0800

    add Longest Palindromic Substring

---
+++ src/main/java/org/zhenchao/leetcode/util/ArrayUtils.java
@@ -0,0 +1,17 @@
+package org.zhenchao.leetcode.util;
+
+/**
+ * @author zhenchao.wang 2017-04-25 21:32
+ * @version 1.0.0
+ */
+public class ArrayUtils {
+
+    public static void display(boolean[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print((array[i][j] ? 1 : 0) + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
