baa93b8bad8d071fd1c7ebd9d25356b86e8112fd
wangzhenchao <wangzhenchao@xiaomi.com>
2017-04-28 19:49:18 +0800

    wa: three sum closest

--- src/main/java/org/zhenchao/leetcode/medium/ThreeSumClosest.java
+++ src/main/java/org/zhenchao/leetcode/medium/ThreeSumClosest.java
@@ -29,25 +29,30 @@ public class ThreeSumClosest {
     }
 
     public int threeSumClosest(int[] nums, int target) {
+        int basic = nums[0] + nums[1] + nums[2];
+        if (nums.length == 3) {
+            return basic;
+        }
         Arrays.sort(nums);
-        int min = nums[0] + nums[1] + nums[2];
+        int min = Math.abs(target - basic);
         int result = min;
         int pre = nums[0];
         for (int i = 0; i < nums.length - 2; i++) {
             if (i > 0 && nums[i] == pre) continue;
-            int closest = this.twoSum(nums, i + 1, nums[i] - target) + nums[i];
+            int closest = this.twoSum(nums, i + 1, target - nums[i]) + nums[i];
             if (Math.abs(target - closest) < min) {
                 min = Math.abs(target - closest);
                 result = closest;
             }
-            min = Math.min(min, nums[i] + closest);
         }
         return result;
     }
 
     public static void main(String[] args) {
         ThreeSumClosest tsc = new ThreeSumClosest();
-        int[] nums = {-1, 2, 1, -4};
+        // int[] nums = {-1, 2, 1, -4};
+        // int[] nums = {0, 0, 0};
+        int[] nums = {1, 1, 1, 1};
         System.out.println(tsc.threeSumClosest(nums, 1));
     }
 
