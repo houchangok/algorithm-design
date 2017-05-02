f4836acb69d3ef78e7aa2ff80277a8119ac13b03
zhenchao <zhenchao.wang@hotmail.com>
2017-05-02 21:26:37 +0800

    TODO: Divide Two Integers

---
+++ src/main/java/org/zhenchao/leetcode/medium/DivideTwoIntegers.java
@@ -0,0 +1,24 @@
+package org.zhenchao.leetcode.medium;
+
+/**
+ * No.29 Divide Two Integers
+ *
+ * @author zhenchao.wang 2017-05-02 21:24
+ * @version 1.0.0
+ */
+public class DivideTwoIntegers {
+
+    /**
+     * 不能使用乘、除，取模
+     *
+     * @param dividend 被除数
+     * @param divisor 除数
+     * @return
+     */
+    public int divide(int dividend, int divisor) {
+
+        return -1;
+
+    }
+
+}
