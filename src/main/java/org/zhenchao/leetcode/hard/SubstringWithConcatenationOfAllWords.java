5cba517aa067cf4db14b154895ade9adb0ce8683
zhenchao <zhenchao.wang@hotmail.com>
2017-05-03 21:47:34 +0800

    add Substring with Concatenation of All Words

---
+++ src/main/java/org/zhenchao/leetcode/hard/SubstringWithConcatenationOfAllWords.java
@@ -0,0 +1,18 @@
+package org.zhenchao.leetcode.hard;
+
+import java.util.List;
+
+/**
+ * No.30 Substring with Concatenation of All Words
+ *
+ * @author zhenchao.wang 2017-05-03 21:44
+ * @version 1.0.0
+ */
+public class SubstringWithConcatenationOfAllWords {
+
+    public List<Integer> findSubstring(String s, String[] words) {
+
+        return null;
+    }
+
+}
