5029ae0196ebaed12fd451c2d6d497f892ac496b
zhenchao <zhenchao.wang@hotmail.com>
2017-05-04 21:42:57 +0800

    add next permutation

--- src/main/java/org/zhenchao/leetcode/hard/SubstringWithConcatenationOfAllWords.java
+++ src/main/java/org/zhenchao/leetcode/hard/SubstringWithConcatenationOfAllWords.java
@@ -1,5 +1,6 @@
 package org.zhenchao.leetcode.hard;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,8 +12,58 @@ import java.util.List;
 public class SubstringWithConcatenationOfAllWords {
 
     public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < words.length; i++) {
+            list.add(this.strStr(s, words[i]));
+        }
+        return list;
+    }
 
-        return null;
+    public int strStr(String haystack, String needle) {
+        int lenHay = haystack.length(), lenNeed = needle.length();
+        if (lenNeed > lenHay) {
+            return -1;
+        }
+        if (lenNeed == 0) {
+            return 0;
+        }
+        int i = 0, j = 0;
+        int[] next = this.next(needle);
+        while (i < lenHay && j < lenNeed) {
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                i++; j++;
+            } else if (j == 0) {
+                i++;
+            } else {
+                j = next[j];
+            }
+        }
+        if (j == lenNeed) {
+            return i - lenNeed;
+        }
+        return -1;
+    }
+
+    private int[] next(String needle) {
+        int[] next = new int[needle.length()];
+        next[0] = -1; // 首字母的next值始终为-1
+        int k = -1;
+        for (int i = 1; i < needle.length(); ) {
+            if (k == -1 || needle.charAt(i - 1) == needle.charAt(k)) {
+                next[i] = ++k;
+                i++;
+            } else {
+                k = next[k];
+            }
+        }
+        return next;
+    }
+
+    public static void main(String[] args) {
+        String s = "barfoothefoobarman";
+        String[] words = {"foo", "bar"};
+        SubstringWithConcatenationOfAllWords scw = new SubstringWithConcatenationOfAllWords();
+        System.out.println(scw.findSubstring(s, words));
     }
 
 }
---
+++ src/main/java/org/zhenchao/leetcode/medium/NextPermutation.java
@@ -0,0 +1,15 @@
+package org.zhenchao.leetcode.medium;
+
+/**
+ * No.31 Next Permutation
+ *
+ * @author zhenchao.wang 2017-05-04 21:39
+ * @version 1.0.0
+ */
+public class NextPermutation {
+
+    public void nextPermutation(int[] nums) {
+
+    }
+
+}
