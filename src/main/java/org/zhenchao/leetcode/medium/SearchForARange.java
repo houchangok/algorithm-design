563d972ad93359763f581b80a358ab7ffc85ba4a
zhenchao <zhenchao.wang@hotmail.com>
2017-05-08 22:53:34 +0800

    add search for a range

---
+++ src/main/java/org/zhenchao/leetcode/medium/SearchForARange.java
@@ -0,0 +1,16 @@
+package org.zhenchao.leetcode.medium;
+
+/**
+ * No.34 Search for a Range
+ *
+ * @author zhenchao.wang 2017-05-08 22:52
+ * @version 1.0.0
+ */
+public class SearchForARange {
+
+    public int[] searchRange(int[] nums, int target) {
+
+        return null;
+    }
+
+}
