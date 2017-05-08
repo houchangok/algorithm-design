78542db2e781781b3a4897c3e1e0ca8465fa7195
wangzhenchao <wangzhenchao@xiaomi.com>
2017-05-08 19:25:48 +0800

    AC: Search in Rotated Sorted Array

--- src/main/java/org/zhenchao/leetcode/medium/SearchInRotatedSortedArray.java
+++ src/main/java/org/zhenchao/leetcode/medium/SearchInRotatedSortedArray.java
@@ -1,5 +1,7 @@
 package org.zhenchao.leetcode.medium;
 
+import java.util.Arrays;
+
 /**
  * No.33 Search in Rotated Sorted Array
  *
@@ -8,9 +10,50 @@ package org.zhenchao.leetcode.medium;
  */
 public class SearchInRotatedSortedArray {
 
+    /**
+     * (l, m, r) 分别表示左、中、右
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
     public int search(int[] nums, int target) {
+        if (nums.length == 0) return -1;
+        int result = this.search(nums, 0, nums.length - 1, target);
+        return result < 0 ? -1 : result;
+    }
 
-        return -1;
+    public int search(int[] nums, int left, int right, int target) {
+        if (left == right) {
+            return target == nums[left] ? left : -1;
+        }
+        int middle = (left + right) / 2;
+        if (nums[middle] == target) {
+            return middle;
+        } else if (nums[left] <= nums[middle]) {
+            // 说明left到middle是有序的
+            if (nums[left] <= target && target < nums[middle]) {
+                // 执行二分查找
+                return Arrays.binarySearch(nums, left, middle, target);
+            } else {
+                return this.search(nums, middle + 1, right, target);
+            }
+        } else {
+            // 说明middle到right是有序的
+            if (nums[middle] < target && target <= nums[right]) {
+                return Arrays.binarySearch(nums, middle + 1, right + 1, target);
+            } else {
+                return this.search(nums, left, middle - 1, target);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        SearchInRotatedSortedArray sirs = new SearchInRotatedSortedArray();
+        // int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        // int[] nums = {1, 2, 4, 5, 6, 7, 0};
+        int[] nums = {1, 3, 5};
+        System.out.println(sirs.search(nums, 2));
     }
 
 }
