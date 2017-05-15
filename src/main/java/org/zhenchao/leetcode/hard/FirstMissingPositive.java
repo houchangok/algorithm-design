c4d26045d4bde9ff11c75197f720091b049231fe
wangzhenchao <wangzhenchao@xiaomi.com>
2017-05-15 18:33:22 +0800

    ADD First Missing Positive

---
+++ src/main/java/org/zhenchao/leetcode/hard/FirstMissingPositive.java
@@ -0,0 +1,15 @@
+package org.zhenchao.leetcode.hard;
+
+/**
+ * No.41 First Missing Positive
+ *
+ * @author zhenchao.wang 2017-05-15 16:39
+ * @version 1.0.0
+ */
+public class FirstMissingPositive {
+
+    public int firstMissingPositive(int[] nums) {
+        return -1;
+    }
+
+}
