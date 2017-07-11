539acc64871de2f3c08d989cb61ecdc5345e4935
zhenchao <zhenchao.wang@hotmail.com>
2017-07-11 21:02:15 +0800

    AC 97

--- src/main/java/org/zhenchao/leetcode/hard/InterleavingString.java
+++ src/main/java/org/zhenchao/leetcode/hard/InterleavingString.java
@@ -9,6 +9,8 @@ package org.zhenchao.leetcode.hard;
 public class InterleavingString {
 
     /**
+     * 动态规划
+     *
      * @param s1
      * @param s2
      * @param s3
@@ -37,39 +39,10 @@ public class InterleavingString {
         return flag[s1.length()][s2.length()];
     }
 
-    public boolean isInterleave2(String s1, String s2, String s3) {
-        if (s3.length() != s1.length() + s2.length()) {
-            return false;
-        }
-
-        boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
-        dp[0][0] = true;
-
-        for (int i = 1; i <= s1.length() && s1.charAt(i - 1) == s3.charAt(i - 1); i++)
-            dp[i][0] = true;
-
-        for (int i = 1; i <= s2.length() && s2.charAt(i - 1) == s3.charAt(i - 1); i++)
-            dp[0][i] = true;
-
-        for (int i = 1; i <= s1.length(); i++) {
-            for (int j = 1; j <= s2.length(); j++) {
-                char c = s3.charAt(i + j - 1);
-                if (c == s1.charAt(i - 1) && dp[i - 1][j]) {
-                    dp[i][j] = true;
-                }
-
-                if (c == s2.charAt(j - 1) && dp[i][j - 1]) {
-                    dp[i][j] = true;
-                }
-            }
-        }
-        return dp[s1.length()][s2.length()];
-    }
-
     public static void main(String[] args) {
         InterleavingString is = new InterleavingString();
-        String s1 = "aabcc", s2 = "abbca", s3 = "aadbbcbcac";
-        System.out.println(is.isInterleave2(s1, s2, s3));
+        String s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac";
+        System.out.println(is.isInterleave(s1, s2, s3));
     }
 
 }
---
+++ src/main/java/org/zhenchao/leetcode/medium/ValidateBinarySearchTree.java
@@ -0,0 +1,18 @@
+package org.zhenchao.leetcode.medium;
+
+import org.zhenchao.leetcode.basic.TreeNode;
+
+/**
+ * 98. Validate Binary Search Tree
+ *
+ * @author zhenchao.wang 2017-07-11 20:57
+ * @version 1.0.0
+ */
+public class ValidateBinarySearchTree {
+
+    public boolean isValidBST(TreeNode root) {
+
+        return false;
+    }
+
+}
