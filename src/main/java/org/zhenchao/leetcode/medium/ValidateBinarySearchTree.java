ab7f6e89c84f88cb5570fb71a88ec5dc5e92166f
wangzhenchao <wangzhenchao@xiaomi.com>
2017-07-13 15:59:11 +0800

    add 98

--- src/main/java/org/zhenchao/leetcode/medium/ValidateBinarySearchTree.java
+++ src/main/java/org/zhenchao/leetcode/medium/ValidateBinarySearchTree.java
@@ -10,9 +10,26 @@ import org.zhenchao.leetcode.basic.TreeNode;
  */
 public class ValidateBinarySearchTree {
 
+    /**
+     * 采用中序遍历
+     *
+     * @param root
+     * @return
+     */
     public boolean isValidBST(TreeNode root) {
+        if (null == root) return false;
+        return this.inorderTraversal(root, null);
+    }
 
-        return false;
+    private boolean inorderTraversal(TreeNode node, TreeNode pre) {
+        if (null != node.left) return this.inorderTraversal(node.left, pre);
+        if (null != pre && pre.val > node.val) {
+            return false;
+        } else {
+            pre = node;
+        }
+        if (null != node.right) return this.inorderTraversal(node.right, pre);
+        return true;
     }
 
 }
