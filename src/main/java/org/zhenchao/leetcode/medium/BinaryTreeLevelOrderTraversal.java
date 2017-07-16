94a26af73a421c07c94b874e85c5993ebee8f647
zhenchao <zhenchao.wang@hotmail.com>
2017-07-16 16:23:22 +0800

    add binary tree level order traversal

---
+++ src/main/java/org/zhenchao/leetcode/medium/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,19 @@
+package org.zhenchao.leetcode.medium;
+
+import org.zhenchao.leetcode.basic.TreeNode;
+
+import java.util.List;
+
+/**
+ * 102. Binary Tree Level Order Traversal
+ *
+ * @author zhenchao.wang 2017-07-16 16:22
+ * @version 1.0.0
+ */
+public class BinaryTreeLevelOrderTraversal {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        return null;
+    }
+
+}
