cfe71ce3197ef47d251acc3aa7599080fdaf4e7f
zhenchao <zhenchao.wang@hotmail.com>
2017-07-16 16:17:42 +0800

    AC 101

--- src/main/java/org/zhenchao/leetcode/easy/SymmetricTree.java
+++ src/main/java/org/zhenchao/leetcode/easy/SymmetricTree.java
@@ -3,22 +3,27 @@ package org.zhenchao.leetcode.easy;
 import org.zhenchao.leetcode.basic.TreeNode;
 
 /**
- * Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+ * 101. Symmetric Tree
  *
- * @author Apache_xiaochao 2015-10-2 15:24:03
+ * @author zhenchao.wang 2015-10-2 15:24:03
+ * @version 1.0.0
  */
 public class SymmetricTree {
 
     /**
      * 递归，每次比较left的left和right的right，以及left的right和right的left
      *
-     * @param left
-     * @param right
-     *
+     * @param root
      * @return
      */
-    public boolean check(TreeNode left, TreeNode right) {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return this.check(root.left, root.right);
+    }
 
+    private boolean check(TreeNode left, TreeNode right) {
         if (left == null && right == null) {
             return true;
         } else if (left == null || right == null) {
@@ -29,13 +34,6 @@ public class SymmetricTree {
                 && this.check(left.left, right.right)
                 && this.check(left.right, right.left);
 
-    }
-
-    public boolean isSymmetric(TreeNode root) {
-        if (root == null) {
-            return true;
-        }
-        return this.check(root.left, root.right);
     }
 
 }
