46de14d9a3004ee658e57214fdc9a4a7ab009967
zhenchao <zhenchao.wang@hotmail.com>
2017-07-17 21:21:39 +0800

    AC: binary tree level order traversal

--- src/main/java/org/zhenchao/leetcode/medium/BinaryTreeLevelOrderTraversal.java
+++ src/main/java/org/zhenchao/leetcode/medium/BinaryTreeLevelOrderTraversal.java
@@ -15,25 +15,34 @@ import java.util.Queue;
  */
 public class BinaryTreeLevelOrderTraversal {
 
+    /**
+     * 存放结点和所在层次
+     */
+    private class Pair {
+        TreeNode node;
+        int level;
+
+        public Pair(TreeNode node, int level) {
+            this.node = node;
+            this.level = level;
+        }
+    }
+
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         if (null == root) return result;
-        Queue<TreeNode> queue = new LinkedList<TreeNode>();
-        queue.add(root);
-        int count = 1, n = 0;
+        Queue<Pair> queue = new LinkedList<Pair>();
+        queue.add(new Pair(root, 0));
         List<Integer> list = new ArrayList<Integer>();
         while (!queue.isEmpty()) {
-            TreeNode node = queue.poll();
-            list.add(node.val);
-            if (count == Math.pow(2, n)) {
+            Pair pair = queue.poll();
+            if (pair.level != result.size()) {
                 result.add(new ArrayList<Integer>(list));
                 list = new ArrayList<Integer>();
-                count = 0;
-                n++;
             }
-            if (null != node.left) queue.add(node.left);
-            if (null != node.right) queue.add(node.right);
-            count++;
+            list.add(pair.node.val);
+            if (null != pair.node.left) queue.add(new Pair(pair.node.left, pair.level + 1));
+            if (null != pair.node.right) queue.add(new Pair(pair.node.right, pair.level + 1));
         }
         if (!list.isEmpty()) result.add(new ArrayList<Integer>(list));
         return result;
@@ -43,9 +52,11 @@ public class BinaryTreeLevelOrderTraversal {
         BinaryTreeLevelOrderTraversal btlot = new BinaryTreeLevelOrderTraversal();
         TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
-        // root.right = new TreeNode(3);
-        // root.left.left = new TreeNode(4);
-        // root.left.right = new TreeNode(5);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        //root.left.left.left = new TreeNode(4);
+        //root.left.left.left.left = new TreeNode(5);
+        root.left.right = new TreeNode(5);
         List<List<Integer>> list = btlot.levelOrder(root);
         for (final List<Integer> l : list) {
             System.out.println(l);
