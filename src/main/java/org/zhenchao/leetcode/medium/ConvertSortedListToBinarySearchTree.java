1b5e3c0175191b40ab788b4d5b3946024cd74240
zhenchao <zhenchao.wang@hotmail.com>
2017-07-23 22:20:32 +0800

    add 109

---
+++ src/main/java/org/zhenchao/leetcode/medium/ConvertSortedListToBinarySearchTree.java
@@ -0,0 +1,18 @@
+package org.zhenchao.leetcode.medium;
+
+import org.zhenchao.leetcode.basic.ListNode;
+import org.zhenchao.leetcode.basic.TreeNode;
+
+/**
+ * 109. Convert Sorted List to Binary Search Tree
+ *
+ * @author zhenchao.wang 2017-07-23 21:34
+ * @version 1.0.0
+ */
+public class ConvertSortedListToBinarySearchTree {
+
+    public TreeNode sortedListToBST(ListNode head) {
+        return null;
+    }
+
+}
