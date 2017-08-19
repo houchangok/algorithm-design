099c08d4639863c5c5a0083e15e0fb7f1f0b3e4a
wangzhenchao <wangzhenchao@xiaomi.com>
2017-08-19 11:19:48 +0800

    AC：138, 141, 142, 146, 147

--- src/main/java/org/zhenchao/leetcode/hard/LRUCache.java
+++ src/main/java/org/zhenchao/leetcode/hard/LRUCache.java
@@ -4,11 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and set.
- * get(key) - Get the val (will always be positive) of the key if the key exists in the cache, otherwise return -1.
- * set(key, val) - Set or insert the val if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+ * 146. LRU Cache
  *
- * @author Apache_xiaochao 2015-10-8 18:56:48
+ * @author zhenchao.wang 2015-10-8 18:56:48
+ * @version 1.0.0
  */
 public class LRUCache {
 
@@ -52,11 +51,7 @@ public class LRUCache {
         return node.data;
     }
 
-    /**
-     * @param key
-     * @param value
-     */
-    public void set(int key, int value) {
+    public void put(int key, int value) {
         Node node = this.keys.get(key);
         if (node != null && node.data == value) {
             return;
@@ -73,7 +68,6 @@ public class LRUCache {
             node.next = this.root.next;
             node.pre = this.root;
             this.root.next = node;
-
         } else {
             // 结点不存在
             if (this.keys.size() < this.capacity) {
@@ -88,7 +82,6 @@ public class LRUCache {
 
                 // 往hash表添加一个当前结点的句柄
                 this.keys.put(key, node);
-
             } else {
                 // 删除最近最少使用的那个结点
                 Node dNode = this.root.pre;
@@ -109,18 +102,11 @@ public class LRUCache {
 
                 // 添加新的结点的句柄到HashMap
                 this.keys.put(key, node);
-
             }
         }
     }
-
 }
 
-/**
- * 自定义双向链表结点
- *
- * @author Apache_xiaochao 2015-10-8 19:18:46
- */
 class Node {
     int key;
     int data;
