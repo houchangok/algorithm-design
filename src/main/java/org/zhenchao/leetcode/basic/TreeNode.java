34a1c9d9bfca61f78239ba85cc207fbe559c8f23
wangzhenchao <wangzhenchao@xiaomi.com>
2017-09-23 13:37:17 +0800

    AC: 199, 200, 202, 203

--- src/main/java/org/zhenchao/leetcode/basic/TreeNode.java
+++ src/main/java/org/zhenchao/leetcode/basic/TreeNode.java
@@ -17,4 +17,8 @@ public class TreeNode {
         this.val = val;
     }
 
+    @Override
+    public String toString() {
+        return "val=" + val;
+    }
 }
---
+++ src/main/java/org/zhenchao/leetcode/easy/CountPrimes.java
@@ -0,0 +1,16 @@
+package org.zhenchao.leetcode.easy;
+
+/**
+ * 204. Count Primes
+ *
+ * @author zhenchao.wang 2017-09-23 12:45
+ * @version 1.0.0
+ */
+public class CountPrimes {
+
+    public int countPrimes(int n) {
+
+        return 0;
+    }
+
+}
