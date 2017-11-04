1dcd220727cb1ff64582d51586dd3df573efd06b
wangzhenchao <wangzhenchao@xiaomi.com>
2017-11-04 12:48:32 +0800

    add spiral matrix

---
+++ src/main/java/org/zhenchao/leetcode/medium/SpiralMatrixII.java
@@ -0,0 +1,17 @@
+package org.zhenchao.leetcode.medium;
+
+/**
+ * 59. Spiral Matrix II
+ *
+ * @author zhenchao.wang 2017-11-04 12:10
+ * @version 1.0.0
+ */
+public class SpiralMatrixII {
+
+    public int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+
+        return matrix;
+    }
+
+}
